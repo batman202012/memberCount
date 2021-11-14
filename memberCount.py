f89b4de7a0f290d67873fe4b3fb3f444516c81fb
Joseph <jnjbros2@gmail.com>
2021-11-13 23:25:01 -0800

    Add files via upload

---
+++ memberCount.py
@@ -0,0 +1,43 @@
+from typing import Literal
+from redbot.core import commands
+from redbot.core import checks
+from redbot.core.bot import Red
+from redbot.core.config import Config
+import discord
+import asyncio
+
+class memberCount(commands.Cog):
+     """
+     Member Count Cog
+     """
+
+     def __init__(self, bot: Red) -> None:
+          self.bot = bot
+          self.config = Config.get_conf(
+               self,
+               identifier=None,
+               force_registration=True,
+          )
+    
+     async def members(self, ctx):
+          activeGuilds = self.bot.guilds
+          channel = self.bot.get_channel(909268842486824960)
+          sum = 0
+          for s in activeGuilds:
+              sum += len(s.members)
+          await channel.edit(name='❎ MEMBERS: {} ❎'.format(int(sum)))
+          print("count adjusted")
+          await asyncio.sleep(1)
+          
+     @commands.Cog.listener()
+     async def on_member_join(self, member):
+          print("joined")
+          await members()
+
+     @commands.Cog.listener()
+     async def on_member_remove(self, member):
+          print("left")
+          await members()
+    
+
+                    
